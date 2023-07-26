1234916c5ae84b17284bb80add3d521d02a5f5d1
Oleg Vishnevskiy <vishnevskiyoleg@gmail.com>
2023-07-26 09:53:19 +0300

    rules

--- app/src/main/java/music/musicplay/ui/theme/WhimsicalScene.kt
+++ app/src/main/java/music/musicplay/ui/theme/WhimsicalScene.kt
@@ -1,10 +1,40 @@
 package music.musicplay.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import music.musicplay.R
 
 
 @Composable
 fun WhimsicalScene(){
+
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(
+            painter = painterResource(id = R.drawable.surface),
+            contentDescription = "",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Text(
+            text = "Rules textr",
+            color = CrazyWhite,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.Center)
+        )
+    }
 
 
 
